0d2e147bdfb4462eeb5bf5255559d17204ac0b3b
milkiq <17810390+milkiq@users.noreply.github.com>
2024-01-13 19:41:45 +0800

    更新方向光demo

--- node_3d.gd
+++ node_3d.gd
@@ -1,5 +1,6 @@
 extends Node3D
 
+var postMaterial = load("res://post.tres")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,9 +11,19 @@ func _ready():
 	var texture:Texture2DRD= Texture2DRD.new()
 	texture.texture_rd_rid=rid
 	$CanvasLayer/Custom_0.texture=texture
+
+
+	buffer.set_meta("post_process_material", postMaterial.get_rid())
+	rid = buffer.get_texture("render_buffers","custom_color")
+	texture = Texture2DRD.new()
+	texture.texture_rd_rid=rid
+	$CanvasLayer/Custom_1.texture=texture
 	print(rid)
 
-
+	rid = buffer.get_texture("render_buffers","position")
+	texture = Texture2DRD.new()
+	texture.texture_rd_rid=rid
+	$CanvasLayer/Custom_2.texture=texture
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
