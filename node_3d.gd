37e36cfa10ef36cc4216851d5d42325a6ab82a2f
milkiq <17810390+milkiq@users.noreply.github.com>
2024-01-15 23:29:09 +0800

    更新demo，用于测试shader新增参数

--- node_3d.gd
+++ node_3d.gd
@@ -1,29 +1,33 @@
 extends Node3D
 
-var postMaterial = load("res://post.tres")
+var deferredMaterial = load("res://deferred.tres")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	await  get_tree().process_frame
-	await  get_tree().process_frame
-	var buffer :RenderSceneBuffersRD =get_viewport().get_render_buffer()
-	var rid = buffer.get_texture("render_buffers","custom_0")
-	var texture:Texture2DRD= Texture2DRD.new()
-	texture.texture_rd_rid=rid
-	$CanvasLayer/Custom_0.texture=texture
+	for _i in 2:
+		await get_tree().process_frame
 
+	var buffer: RenderSceneBuffersRD
+	var rid: RID
+	var texture: Texture2DRD
+	buffer = get_viewport().get_render_buffer()
+	buffer.set_meta("deferred_process_material", deferredMaterial.get_rid())
 
-	buffer.set_meta("post_process_material", postMaterial.get_rid())
-	rid = buffer.get_texture("render_buffers","custom_color")
+	#法线贴图是-1到1，无法直接渲染
+	#rid = buffer.get_texture("render_buffers", "custom_0")
+	#texture = Texture2DRD.new()
+	#texture.texture_rd_rid = rid
+	#$CanvasLayer/Custom_0.texture = texture
+
+	rid = buffer.get_texture("render_buffers", "custom_color")
 	texture = Texture2DRD.new()
-	texture.texture_rd_rid=rid
-	$CanvasLayer/Custom_1.texture=texture
-	print(rid)
+	texture.texture_rd_rid = rid
+	$CanvasLayer/Custom_1.texture = texture
 
-	rid = buffer.get_texture("render_buffers","position")
+	rid = buffer.get_texture("render_buffers", "defer_position")
 	texture = Texture2DRD.new()
-	texture.texture_rd_rid=rid
-	$CanvasLayer/Custom_2.texture=texture
+	texture.texture_rd_rid = rid
+	$CanvasLayer/Custom_2.texture = texture
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
